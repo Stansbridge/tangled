58d195746e4abd02f88200f12d3d524c1d36128b
whizzer0 <whizzerZero@gmail.com>
2017-08-22 15:35:31 +0100

    Fully implemented new input (incl. mouse and axes); convert cooldowns to new system next

--- controls.py
+++ controls.py
@@ -1,7 +1,7 @@
 import pygame, time
 import pygame.locals
 
-INPUT_TIMEOUT_DEFAULT = 150
+INPUT_TIMEOUT_DEFAULT = 0.15
 
 class InputHandler(): 
     def __init__(self):
@@ -16,7 +16,7 @@ class InputHandler():
                                 "special":[pygame.locals.K_BACKQUOTE],                                      #i.e. Y
                                 "start":[pygame.locals.K_ESCAPE],
                                 "select":[]}
-        self.bindingsJoystick = {"up":[],
+        self.bindingsJoystick = {"up":[], #These are handled by hardcoded axis events
                                 "down":[],
                                 "left":[],
                                 "right":[],
@@ -26,6 +26,16 @@ class InputHandler():
                                 "special":[3],   #i.e. Y
                                 "start":[7],
                                 "select":[6]}
+        self.bindingsMouse = {"up":[],
+                            "down":[],
+                            "left":[],
+                            "right":[],
+                            "enter":[1],
+                            "back":[3],
+                            "change":[4,5,8],
+                            "special":[9],
+                            "start":[],
+                            "select":[]}
         self.inputsPressed = {"up":time.time(),
                             "down":time.time(),
                             "left":time.time(),
@@ -69,19 +79,37 @@ class InputHandler():
     #To use the check functions, you still need to pass the event yourself as the InputHandler can't poll for them. Use `for event in pygame.event.get():` to do this.
     def checkHold(self, event): #Use this one if you don't care how often the input is being pressed.
         returned = False
-        if event.type == pygame.locals.KEYDOWN:
+        if event.type == pygame.locals.KEYDOWN: #Keyboard key pressed
             for keysID, keys in self.bindingsKeyboard.items():
                 if event.key in keys:
                     returned = keysID
                     self.inputsPressed[keysID] = time.time() + self.inputsTimeout[keysID]
                     pygame.event.clear(pygame.locals.KEYDOWN)
                     break
-        elif event.type == pygame.locals.JOYBUTTONDOWN:
+        elif event.type == pygame.locals.JOYBUTTONDOWN: #Gamepad button pressed
             for buttonsID, buttons in self.bindingsJoystick.items():
                 if event.button in buttons:
                     returned = buttonsID
                     self.inputsPressed[buttonsID] = time.time() + self.inputsTimeout[buttonsID]
                     pygame.event.clear(pygame.locals.JOYBUTTONDOWN)
+                    break
+        elif event.type == pygame.locals.JOYAXISMOTION: #Gamepad d-pad or joystick moved (note: these bindings are hardcoded)
+            if event.axis == 0: #X axis
+                if event.value > 0:
+                    returned = "right"
+                elif event.value < 0:
+                    returned = "left"
+            elif event.axis == 1: #Y axis
+                if event.value > 0:
+                    returned = "up"
+                elif event.value < 0:
+                    returned = "down"
+        elif event.type == pygame.locals.MOUSEBUTTONDOWN:
+            for buttonsID, buttons in self.bindingsMouse.items():
+                if event.button in buttons:
+                    returned = buttonsID
+                    self.inputsPressed[buttonsID] = time.time() + self.inputsTimeout[buttonsID]
+                    pygame.event.clear(pygame.locals.MOUSEBUTTONDOWN)
                     break
         return returned
     
@@ -102,5 +130,30 @@ class InputHandler():
                         returned = buttonsID
                     self.inputsPressed[buttonsID] = time.time() + self.inputsTimeout[buttonsID]
                     pygame.event.clear(pygame.locals.JOYBUTTONDOWN)
+                    break
+        elif event.type == pygame.locals.JOYAXISMOTION:
+            inputAxis = False
+            if event.axis == 0: #X axis
+                if event.value > 0:
+                    inputAxis = "right"
+                elif event.value < 0:
+                    inputAxis = "left"
+            elif event.axis == 1: #Y axis
+                if event.value > 0:
+                    inputAxis = "up"
+                elif event.value < 0:
+                    inputAxis = "down"
+            if inputAxis:
+                if self.inputsPressed[inputAxis] <= time.time():
+                    self.inputsPressed[inputAxis] = time.time() + self.inputsTimeout[buttonsID]
+                    returned = inputAxis
+            pygame.event.clear(pygame.locals.JOYAXISMOTION)
+        elif event.type == pygame.locals.MOUSEBUTTONDOWN:
+            for buttonsID, buttons in self.bindingsMouse.items():
+                if event.button in buttons:
+                    if self.inputsPressed[buttonsID] <= time.time():
+                        returned = buttonsID
+                    self.inputsPressed[buttonsID] = time.time() + self.inputsTimeout[buttonsID]
+                    pygame.event.clear(pygame.locals.MOUSEBUTTONDOWN)
                     break
         return returned
